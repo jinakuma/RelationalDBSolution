9e30a450462374b1514121b3c87c5226f799d192
Murat Utlu <utlumurat@gmail.com>
2021-08-04 16:29:15 +0300

    Add project files.

---
+++ SQLContactsDB/dbo/tables/EmailAddresses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[EmailAddresses]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[EmailAddress] NVARCHAR(100) NOT NULL
+	
+)
---
+++ SQLServerUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+
+namespace SQLServerUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlCrudOperations sql = new SqlCrudOperations(GetConnectionString());
+            
+            //ReadAllContacts(sql);
+
+            //ReadContact(sql,1);
+            
+            //CreateNewContact(sql);
+
+            //UpdateContact(sql);
+
+            RemovePhoneNumberFromContact(sql,1,1);
+            
+            Console.ReadLine();
+        }
+
+        private static void CreateNewContact(SqlCrudOperations sql)
+        {
+            FullContactModel user = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "Mustafa",
+                    LastName = "Uzun"
+                }
+            };
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "abc@xyz.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { Id = 2, EmailAddress = "me@utlumurat.com" });
+
+            user.PhoneNumbers.Add(new PhoneNumberModel { Id = 1, PhoneNumber = "444-42231123" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-422311" });
+            sql.CreateContact(user);
+        }
+
+        private static void ReadAllContacts(SqlCrudOperations sql)
+        {
+            var rows = sql.GetAllContacts();
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id} : {row.FirstName} {row.LastName}");
+            }
+        }
+
+        private static void UpdateContact(SqlCrudOperations sql)
+        {
+            BasicContactModel contact = new BasicContactModel
+            {
+                Id =1,
+                FirstName = "Murti",
+                LastName = "Utlu"
+            };
+            sql.UpdateContactName(contact);
+
+        }
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            
+            return config.GetConnectionString(connectionStringName);
+        }
+        private static void ReadContact(SqlCrudOperations sql, int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+            
+                Console.WriteLine($"{contact.BasicInfo.Id} : {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+            
+        }
+
+        private static void RemovePhoneNumberFromContact(SqlCrudOperations sql, int contactId, int phoneNumberId)
+        {
+            sql.RemovePhoneNumberFromContact(contactId,phoneNumberId);
+        }
+    }
+}
